5cff0b8fe2c0354768b611943b8a68288e8d2b47
PatTheHyruler <pmcroofplays@gmail.com>
2021-04-28 02:18:57 +0300

    increased beatsaver request timeout time from 5 to 10 seconds

--- src/beatsaver.py
+++ src/beatsaver.py
@@ -12,7 +12,7 @@ class Beatsaver:
         self._session = cfscrape.create_scraper()
 
     def get_song_by_hash(self, song_hash):
-        response = self._session.get(f"{self._url}/maps/by-hash/{song_hash}", cookies={}, timeout=5)
+        response = self._session.get(f"{self._url}/maps/by-hash/{song_hash}", cookies={}, timeout=10)
         song_info = json.loads(response.content)
 
         return Song(song_info)
