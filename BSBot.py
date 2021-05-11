56d49c3f371fa8df2cff4e251315fef4a10d61ef
PatTheHyruler <pmcroofplays@gmail.com>
2021-05-11 03:41:56 +0300

    changed the way bot commands work moved from using if>elif>else statements to using switch case statements (implemented by using dicts) changed the syntax of the !player command from ´!player add/remove <discord user> <ScoreSaber profile url/id>´ to ´!player add/remove <ScoreSaber profile url/id> <discord user>´, because it's nicer to have optional arguments be at the end

--- BSBot.py
+++ BSBot.py
@@ -16,110 +16,25 @@ client = commands.Bot('!')
 async def on_ready():
     Logger.log_add(f'{client.user} has connected to Discord!')
 
-
-async def format_error_message(message):
+async def invalid_command(message, args=False):
     await message.channel.send('Sorry, something went wrong! Make sure your message is in the correct format.')
 
+async def hello(message, args=False):
+    await message.channel.send('Hello there!')
 
-@client.event
-async def on_message(message):
-    guild = uow.guild_repo.get_guild_by_id(message.channel.guild.id)
+async def help_func(message, args=False):
+    Logger.log_add(f'help_func()')
+    m1 = '**To add/remove player:**\n`!player add/remove <ScoreSaber profile url/id> <discord user>`\n'
+    m2 = '**To add/remove current channel:**\n`!channel add/remove`\n'
+    m3 = '**To add/remove feature from Discord server:**\n`!feature add/remove [ppRoles]`\n'
+    m4 = '**To update data:**\n`!update [players/roles/scores/notifications]`'
+    msg = m1 + m2 + m3 + m4
+    await message.channel.send(msg)
 
-    if guild is None:
-        uow.guild_repo.add_guild(message.channel.guild)
+async def player_func(message, args):
+    Logger.log_add(f'player_func(message.content: {message.content}, args: {args})')
 
-    if message.author == client.user:
-        return
-
-    if message.content.startswith('!hello'):
-        await message.channel.send('Hello there!')
-
-    if message.content.startswith('!help'):
-        pattern = re.compile(r'!help *(\w*)')
-        match = re.match(pattern, message.content)
-
-        if match:
-            action = match.group(1)
-            await help_func(message, action)
-
-    if message.content.startswith('!player'):
-        pattern = re.compile(r"!player *(\w+) (<@!(\d+)>)? *(https?://scoresaber\.com/u/)?(\d{17})")
-        match = re.match(pattern, message.content)
-
-        if match:
-            action = match.group(1)
-            discord_user_id = match.group(3)
-            player_id = str(match.group(5))
-
-            await player_func(message.channel.guild.id, player_id, discord_user_id, message, action)
-        else:
-            await format_error_message(message)
-
-    
-
-    if message.content.startswith("!feature"):
-        # SECURITY
-        if not await can_execute(message):
-            return
-        pattern = re.compile(r'!feature *(\w+) *(\w+)')
-        match = re.match(pattern, message.content)
-
-        if match:
-            action = match.group(1)
-            feature_flag = match.group(2)
-
-            await enable_feature(message, action, feature_flag)
-
-    if message.content.startswith('!channel'):
-        # SECURITY
-        if not await can_execute(message):
-            return
-        pattern = re.compile(r'!channel *(\w+)')
-        match = re.match(pattern, message.content)
-
-        if match:
-            action = match.group(1)
-            await channel_func(message.channel.id, message.channel.guild.id, message, action)
-        else:
-            await format_error_message(message)
-
-    if message.content.startswith("!update"):
-        # SECURITY
-        if not await can_execute(message):
-            return
-        pattern = re.compile(r'!update *(\w+)')
-        match = re.match(pattern, message.content)
-
-        if match:
-            action = match.group(1)
-            await run_task(message.channel.guild, action)
-        else:
-            await run_task(message.channel.guild)
-
-
-async def help_func(message, action):
-    Logger.log_add(f'help_func(action: {action})')
-    if action == "69":
-        await message.channel.send('420')
-    elif action == "ayy":
-        await message.channel.send("lmao")
-    else:
-        m1 = '**To add/remove player:**\n`!player add/remove <discord user> <ScoreSaber profile url>`\n'
-        m2 = '**To add/remove current channel:**\n`!channel add/remove`\n'
-        m3 = '**To add/remove another channel in the same Discord server:**\n`!channel add/remove channelID`\n'
-        m4 = '**To add/remove feature from Discord server:**\n`!feature add/remove [ppRoles]`'
-        m5 = '**To update data:**\n`!update [players/roles/scores/notifications]`'
-        msg = m1 + m2 + m3 + m4 + m5
-        await message.channel.send(msg)
-
-
-async def player_func(guild_id, playerID, discord_user_id, message, action):
-    Logger.log_add(f'player_func(guild_id: {guild_id}, playerID: {playerID}, discord_user_id: {discord_user_id}, action: {action})')
-
-    db_guild = uow.guild_repo.get_guild_by_id(guild_id)
-    db_player = uow.player_repo.get_player_by_player_id(playerID)
-
-    if action == "add":
+    async def add_player():
         if db_player in db_guild.players:
             await message.channel.send(f'Player ID {playerID} has already been added!')
             return
@@ -147,7 +62,8 @@ async def player_func(guild_id, playerID, discord_user_id, message, action):
             await tasks.update_player_roles(db_guild, new_player)
 
         await message.channel.send(f'Player {new_player.playerName} successfully added!')
-    elif action == "remove":
+    
+    async def remove_player():
         is_removed = False
 
         for db_player in db_guild.players:
@@ -161,11 +77,55 @@ async def player_func(guild_id, playerID, discord_user_id, message, action):
             await message.channel.send(f"Player ID {playerID} already doesn't exist in this Discord server's player list.")
 
 
-async def channel_func(channel_id, guild_id, message, action):
-    Logger.log_add(f'channel_func(channel: {channel_id}, guild: {guild_id}, action: {action})')
-    db_guild = uow.guild_repo.get_guild_by_id(guild_id)
+    actiondict = {
+        "add": add_player,
+        "remove": remove_player
+    }
+    action = args[0]
+    actionfunc = actiondict.get(action, False)
+    if not actionfunc:
+        message.channel.send(f'{action} is not an applicable action, possible actions are: {[i for i in actiondict]}')
+        return
+    
+    guild_id = message.channel.guild.id
+    try:
+        pattern = re.compile(r'(https?://scoresaber\.com/u/)?(\d{17})')
+        match = re.match(pattern, args[1])
+        if match:
+            playerID = match.group(2)
+        else:
+            await message.channel.send('ScoreSaber ID/URL in incorrect format.')
+            return
+    except IndexError:
+        await message.channel.send('You need to provide a ScoreSaber profile ID/URL')
+        return
+    
+    try:
+        pattern = re.compile(r'<@!(\d+)>')
+        match = re.match(pattern, args[2])
+        if match:
+            discord_user_id = args[2]
+            # not sure if it is necessary to check if user id is real or exists in current guild
+            # I think that should be handled by other functions (ppRoles function(s)?)
+        else:
+            await message.channel.send(f'Discord ID in incorrect format (received input was: {args[2]}). You can try omitting this argument if you just wanted to add yourself.')
+            return
+    except IndexError:
+        discord_user_id = message.author.id
 
-    if action == "add":
+
+    Logger.log_add(f'player_func(guild_id: {guild_id}, playerID: {playerID}, discord_user_id: {discord_user_id}, action: {action})')
+
+    db_guild = uow.guild_repo.get_guild_by_id(guild_id)
+    db_player = uow.player_repo.get_player_by_player_id(playerID)
+
+    await actionfunc()
+
+async def channel_func(message, args):
+    if not await can_execute(message):
+        return
+    
+    async def add_channel():
         if db_guild.recent_scores_channel_id is not None:
             await message.channel.send(f'Channel ID {message.channel.name} ({channel_id}) has already been added!')
             return
@@ -175,48 +135,131 @@ async def channel_func(channel_id, guild_id, message, action):
         await tasks.mark_all_guild_scores_sent(db_guild)
 
         await message.channel.send(f'Channel ID {message.channel.name} ({channel_id}) successfully added!')
-    elif action == "remove":
+    async def remove_channel():
         if db_guild.recent_scores_channel_id is None:
             await message.channel.send(f"Channel ID {message.channel.name} ({channel_id}) already doesn't exist in the channel list for server {client.get_guild(guild_id)}.")
 
         uow.guild_repo.set_recent_score_channel_id(guild_id, None)
         await message.channel.send(f'Channel ID {message.channel.name} ({channel_id}) successfully removed!')
-    else:
-        await message.channel.send("Usage: !channel add/remove")
 
+    actiondict = {
+        "add": add_channel,
+        "remove": remove_channel
+    }
+    action = args[0]
+    actionfunc = actiondict.get(action, False)
+    if not actionfunc:
+        message.channel.send(f'{action} is not an applicable action, possible actions are: {[i for i in actiondict]}')
+        return
+    
+    channel_id = message.channel.id
+    guild_id = message.channel.guild.id
 
-async def enable_feature(message, action, feature_flag):
-    guild = message.channel.guild
+    Logger.log_add(f'channel_func(channel: {channel_id}, guild: {guild_id}, action: {action})')
+    db_guild = uow.guild_repo.get_guild_by_id(guild_id)
 
-    if action == "add":
+    await actionfunc()
+
+#async def enable_feature(message, action, feature_flag):
+async def enable_feature(message, args):
+    if not await can_execute(message):
+        return
+    
+    async def add_feature():
         if feature_flag == "ppRoles":
             uow.guild_repo.set_feature(guild.id, feature_flag, True)
             await tasks.update_all_player_roles(guild)
             await message.channel.send("Enabled pp roles feature")
-
-    elif action == "remove":
+    
+    async def remove_feature():
         if feature_flag == "ppRoles":
             uow.guild_repo.set_feature(guild.id, feature_flag, False)
             await tasks.remove_guild_roles(guild)
             await message.channel.send("Disabled pp roles feature")
+
+    actiondict = {
+        "add": add_feature,
+        "remove": remove_feature
+    }
+    action = args[0]
+    actionfunc = actiondict.get(action, False)
+    if not actionfunc:
+        await message.channel.send(f'{action} is not an applicable action, possible actions are: {[i for i in actiondict]}')
+        return
+    
+    featurelist = [
+        "ppRoles"
+    ]
+    feature_flag = args[1]
+    if feature_flag not in featurelist:
+        await message.channel.send(f'{feature_flag} is not an applicable action, possible actions are: {[i for i in featurelist]}')
+        return
+    
+    guild = message.channel.guild
+
+    await actionfunc()
+
+async def run_task(message, args=False):
+    if not await can_execute(message):
+        return
+
+    actiondict = {
+        "players": tasks.update_players,
+        "roles": tasks.update_all_player_roles,
+        "scores": tasks.update_players_scores,
+        "notifications": tasks.send_notifications
+    }
+    if args:
+        action = args[0]
+        actionfunc = actiondict.get(action, False)
+        if not actionfunc:
+            await message.channel.send(f'{action} is not an applicable action, possible actions are: {[i for i in actiondict]}')
+            return
     else:
-        await message.channel.send("Usage: !feature add/remove [ppRoles]")
+        async def actionfunc(guild):
+            await tasks.update_players(guild)
+            await tasks.update_all_player_roles(guild)
+            await tasks.update_players_scores(guild)
+            await tasks.send_notifications(guild)
+
+    Logger.log_add(f'update(guild: {message.guild}, args: {args})')
+    await actionfunc(message.guild)
 
 
-async def run_task(guild, action=None):
-    if action == "players":
-        await tasks.update_players(guild)
-    elif action == "roles":
-        await tasks.update_all_player_roles(guild)
-    elif action == "scores":
-        await tasks.update_players_scores(guild)
-    elif action == "notifications":
-        await tasks.send_notifications(guild)
+async def command(message, command, args):
+    command_dict = {
+        "hello": [hello, 0],
+        "help": [help_func, 0],
+        "player": [player_func, 2],
+        "channel": [channel_func, 1],
+        "feature": [enable_feature, 2],
+        "update": [run_task, 0]
+    }
+    func, required_args = [i for i in command_dict.get(command, invalid_command)]
+    if len(args) >= required_args:
+        await func(message, args)
     else:
-        await tasks.update_players(guild)
-        await tasks.update_all_player_roles(guild)
-        await tasks.update_players_scores(guild)
-        await tasks.send_notifications(guild)
+        await message.channel.send(f'The {command} command requires {required_args} arguments')
+        
+
+@client.event
+async def on_message(message):
+    guild = uow.guild_repo.get_guild_by_id(message.channel.guild.id)
+
+    if guild is None:
+        uow.guild_repo.add_guild(message.channel.guild)
+
+    if message.author == client.user:
+        return
+
+    pattern = re.compile(r'!(\w+) *(\S*) *(\S*) *(\S*)')
+    
+    if message.content.startswith('!'):
+        match = re.match(pattern, message.content)
+        if match:
+            command_msg, *args = [match.group(i+1) for i in range(3) if bool(match.group(i+1))]
+            await command(message, command_msg, args)
+
 
 
 async def can_execute(message):
