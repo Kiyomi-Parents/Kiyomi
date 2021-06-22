5d0515119cf188915d99ec231eda5337fc6795cf
PatTheHyruler <pmcroofplays@gmail.com>
2021-06-22 22:10:48 +0300

    Added !recentsong command

--- src/cogs/beatsaber/message.py
+++ src/cogs/beatsaber/message.py
@@ -12,6 +12,35 @@ from src.cogs.beatsaber.leaderboard.leaderboard_score import LeaderboardScore
 class Message:
 
     @staticmethod
+    def get_score_embed(player, score):
+        song = score.song
+        embed = Embed()
+
+        embed.set_author(name=player.playerName, url=player.profile_url, icon_url=player.avatar_url)
+        #TODO: maybe add a thing that also shows the score's current rank?
+        embed.title = f"#{score.rank} (at the time of setting) for {score.song_name_full} on {score.difficulty_name}"
+        
+        if song is not None:
+            embed.description = F"Mapped by {song.author}"
+
+        embed.add_field(name="PP (at the time of setting)", value=f"**{round(score.pp, 2)}pp** _({score.weighted_pp}pp)_")
+        embed.add_field(name="Accuracy", value=f"**{score.accuracy}%**")
+        embed.add_field(name="Score", value=f"{score.score}")
+
+        if score.mods:
+            embed.add_field(name="Modifiers", value=f"{score.mods}")
+
+        embed.set_thumbnail(url=score.song_image_url)
+        embed.colour = Colour.random(seed=player.playerId)
+        embed.url = score.leaderboard_url
+
+        if song is not None:
+            embed.add_field(name="\u200b", value=f"[Beat Saver]({song.beatsaver_url})")
+            embed.add_field(name="\u200b", value=f"[Preview Map]({song.preview_url})")
+
+        return embed
+
+    @staticmethod
     def get_new_score_embed(player, score, song):
         embed = Embed()
         embed.set_author(name=player.playerName, url=player.profile_url, icon_url=player.avatar_url)
