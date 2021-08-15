2840ef8a6a19537f3fd05e1ee410c606776e9264
LuCkEr <lucker@lucker.xyz>
2021-08-15 18:43:11 +0300

    Merged dev

--- src/cogs/beatsaver/message.py
+++ src/cogs/beatsaver/message.py
@@ -1,31 +1,16 @@
-from discord import Embed, Colour
+from discord import Embed
 
+from src.base.message_builder.beatmap_message_builder import BeatmapMessageBuilder
 from src.cogs.beatsaver.storage.model import Beatmap
 
 
 class Message:
 
     @staticmethod
-    def get_song_embed(beatmap: Beatmap) -> Embed:
-        embed = Embed()
+    def get_song_embed(beatmap: Beatmap):
+        msg = BeatmapMessageBuilder(Embed(), beatmap)
 
-        embed.set_author(name=beatmap.uploader_name, url=beatmap.mapper_url, icon_url=beatmap.uploader_avatar)
-        embed.title = f"{beatmap.name}"
-
-        embed.add_field(name="Rating", value=f"{beatmap.rating}%")
-        embed.add_field(name="Downloads", value=f"{beatmap.stats_downloads}")
-        embed.add_field(name="Length", value=f"{beatmap.length}")
-        embed.add_field(name="BPM", value=f"{beatmap.metadata_bpm}")
-
-        embed.add_field(name="difficulties", value=" ".join(f"**{diff}**" for diff in beatmap.difficulties_short))
-
-        # Should make a simple website that redirects the user to the right links
-        # discord doesn't want to make app links clickable
-        # This will include OneClick links and beatmap download links
-        embed.add_field(name="\u200b", value=f"[Preview Map]({beatmap.preview_url})")
-
-        embed.set_thumbnail(url=beatmap.cover_url)
-        embed.colour = Colour.random(seed=beatmap.uploader_id)
-        embed.url = beatmap.beatsaver_url
+        embed = msg.author().title().rating().downloads(). \
+            length().bpm().diffs().links().thumbnail().url().get_embed()
 
         return embed
--- src/cogs/scoresaber/message.py
+++ src/cogs/scoresaber/message.py
@@ -2,49 +2,16 @@ from discord import Embed, Colour
 
 from .storage.model.player import Player
 from .storage.model.score import Score
+from src.base.message_builder.score_message_builder import ScoreMessageBuilder
 
 
 class Message:
 
     @staticmethod
     def get_score_embed(player: Player, score: Score, country_rank=None):
-        embed = Embed()
+        msg = ScoreMessageBuilder(Embed(), score, player)
 
-        embed.set_author(name=player.player_name, url=player.profile_url, icon_url=player.avatar_url)
-
-        # TODO: maybe add a thing that also shows the score's current rank?
-        if country_rank is None or not isinstance(country_rank, int):
-            embed.title = f"#{score.rank} (at the time of setting) for " \
-                          f"{score.song_name_full} on {score.difficulty_name}"
-        else:
-            embed.title = f"#{score.rank} (#{country_rank} in country) (at the time of setting) for " \
-                          f"{score.song_name_full} on {score.difficulty_name}"
-
-        if score.beatmap_version is not None:
-            embed.description = F"Mapped by {score.beatmap_version.beatmap.metadata_level_author_name}"
-
-        embed.add_field(
-            name="PP (at the time of setting)",
-            value=f"**{round(score.pp, 2)}pp** _({score.weighted_pp}pp)_"
-        )
-        embed.add_field(
-            name="Accuracy",
-            value=f"**{score.accuracy}%**"
-        )
-        embed.add_field(
-            name="Score",
-            value=f"{score.score}"
-        )
-
-        if score.mods:
-            embed.add_field(name="Modifiers", value=f"{score.mods}")
-
-        embed.set_thumbnail(url=score.song_image_url)
-        embed.colour = Colour.random(seed=player.id)
-        embed.url = score.leaderboard_url
-
-        if score.beatmap_version is not None:
-            embed.add_field(name="\u200b", value=f"[Beat Saver]({score.beatmap_version.beatmap.beatsaver_url})")
-            embed.add_field(name="\u200b", value=f"[Preview Map]({score.beatmap_version.beatmap.preview_url})")
+        embed = msg.author().title(country_rank=country_rank).mapper().pp(). \
+            accuracy().score_value().mods().thumbnail().url().beatmap().get_embed()
 
         return embed
