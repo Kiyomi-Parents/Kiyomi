a70b95664402e8a372bb898a22a0cafbbb5b2e3c
LuCkEr <lucker@lucker.xyz>
2021-08-19 07:57:39 +0300

    Fixed broken message for score improvement

--- src/cogs/score_feed/message.py
+++ src/cogs/score_feed/message.py
@@ -12,7 +12,7 @@ class Message:
     def get_new_score_embed(player: Player, score: Score, country_rank=None):
         msg = ScoreMessageBuilder(Embed(), score, player)
 
-        embed = msg.author().title_new(country_rank=country_rank).mapper().pp(). \
+        embed = msg.author().title_new(country_rank).mapper().pp(). \
             accuracy().score_value().mods().thumbnail().url().beatmap().get_embed()
 
         return embed
@@ -21,8 +21,8 @@ class Message:
     def get_improvement_score_embed(player: Player, previous_score: Score, score: Score, country_rank=None):
         msg = ScoreMessageBuilder(Embed(), score, player)
 
-        embed = msg.author().title_improvement(country_rank=country_rank).mapper().pp(previous_score=previous_score). \
-            accuracy(previous_score=previous_score).score_value(previous_score=previous_score). \
+        embed = msg.author().title_improvement(previous_score, country_rank).mapper().pp(previous_score). \
+            accuracy(previous_score).score_value(previous_score). \
             mods().thumbnail().url().beatmap().get_embed()
 
         return embed
