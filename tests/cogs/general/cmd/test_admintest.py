27637414ba7f9df366169856f740bfca0c0d92c9
LuCkEr <lucker@lucker.xyz>
2022-02-04 17:25:28 +0200

    Remove tests

--- tests/cogs/general/cmd/test_admintest.py
+++
@@ -1,8 +0,0 @@
-import pytest
-from discord.ext.test import message, verify
-
-
-@pytest.mark.asyncio
-async def test_admin_test(bot):
-    await message("!admintest")
-    assert verify().message().contains().content("admintest")
