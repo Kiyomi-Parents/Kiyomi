b5563d096eb40f9c6ecf9024db28702978df596b
PatTheHyruler <pmcroofplays@gmail.com>
2022-03-22 13:42:46 +0200

    Start work on making MapPreviewButton use the selected difficulty
    Mostly done, but the actual button in Discord doesn't want to update to the new url.
    Unsure if it's even possible

--- src/cogs/beatsaver/messages/components/buttons/map_preview_button.py
+++ src/cogs/beatsaver/messages/components/buttons/map_preview_button.py
@@ -1,12 +1,15 @@
 import discord
+import pybeatsaver
 
+from src.cogs.beatsaver.storage import Beatmap
 from src.kiyomi import Kiyomi
 
 
 class MapPreviewButton(discord.ui.Button):
-    def __init__(self, bot: Kiyomi, beatmap_id: str):
+    def __init__(self, bot: Kiyomi, beatmap: Beatmap):
         self.bot = bot
-        self.beatmap_id = beatmap_id
+        self.beatmap = beatmap
+        self.difficulty: pybeatsaver.Difficulty = None
 
         super().__init__(
             label="Map Preview",
@@ -14,6 +17,21 @@ class MapPreviewButton(discord.ui.Button):
             url=self.preview_url
         )
 
+    async def update(self, interaction, difficulty: pybeatsaver.Difficulty):
+        self.difficulty = difficulty
+        self._underlying.url = self.preview_url
+        # TODO: Figure out how to make the button actually update
+
     @property
     def preview_url(self) -> str:
-        return f"https://skystudioapps.com/bs-viewer/?id={self.beatmap_id}"
+        result = f"https://skystudioapps.com/bs-viewer/?id={self.beatmap.id}"
+        if self.difficulty is not None:
+            result += f"&difficulty={self.difficulty_index}"
+        return result
+
+    @property
+    def difficulty_index(self):
+        difficulties = [bmv_diff.difficulty.value for bmv_diff in self.beatmap.difficulties]
+        if self.difficulty.value in difficulties:
+            return difficulties.index(self.difficulty.value)
+        return 69  # The preview website will just take the max diff if the given index is higher than the max.
