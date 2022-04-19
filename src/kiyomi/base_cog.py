dc762fc15cfa16b141b3f9a8f61ffa33863b0150
LuCkEr <lucker@lucker.xyz>
2022-04-19 17:04:21 +0300

    Show command params in log

--- src/kiyomi/base_cog.py
+++ src/kiyomi/base_cog.py
@@ -12,11 +12,14 @@ class BaseCog(commands.Cog):
         self.bot = bot
 
     async def cog_before_invoke(self, ctx: ApplicationContext):
+        command_args = [f"{item['name']}: {item['value']}" for item in ctx.interaction.data['options']]
+
         Logger.log(
                 self.qualified_name,
                 f"{colored(ctx.interaction.user.name, 'blue')} executed command "
-                f"{colored('/' + ctx.command.qualified_name, 'blue')} in "
-                f"{colored(ctx.interaction.channel.name, 'blue')} at "
+                f"{colored('/' + ctx.command.qualified_name, 'blue')} "
+                f"{colored(''.join(command_args), 'cyan')} in "
+                f"{colored(f'#{ctx.interaction.channel.name}', 'blue')} at "
                 f"{colored(ctx.interaction.guild.name, 'blue')}"
         )
 
