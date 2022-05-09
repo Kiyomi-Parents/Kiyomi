d9005d2a9488a19cdddb89ffc07f47281b48d36e
LuCkEr <lucker@lucker.xyz>
2022-05-09 11:28:58 +0300

    Convert to async setup for achievements cog

--- src/cogs/achievement/__init__.py
+++ src/cogs/achievement/__init__.py
@@ -6,11 +6,11 @@ from .services.user_achievement_service import UserAchievementService
 from .storage.unit_of_work import UnitOfWork
 
 
-def setup(bot: Kiyomi):
+async def setup(bot: Kiyomi):
     uow = UnitOfWork(bot.database.session)
 
     registry_service = RegistryService(bot, uow)
     user_achievement_service = UserAchievementService(bot, uow, registry_service)
 
-    bot.add_cog(Achievements(bot, user_achievement_service))
-    bot.add_cog(AchievementsAPI(bot, user_achievement_service))
+    await bot.add_cog(Achievements(bot, user_achievement_service))
+    await bot.add_cog(AchievementsAPI(bot, user_achievement_service))
