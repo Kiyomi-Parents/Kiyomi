3813ad5b9fcc2201e64008b6aaa6c7a0138bf7d3
PatTheHyruler <pmcroofplays@gmail.com>
2022-06-09 01:28:16 +0300

    Remove unnecessary None checks, fix an import

--- src/cogs/twitch/arg_resolvers/user_id_resolver.py
+++ src/cogs/twitch/arg_resolvers/user_id_resolver.py
@@ -1,6 +1,6 @@
 from typing import Optional
 
-# from .. import BroadcasterService  # This import breaks everything, which is why it's commented out.
+from ..services.twitch_broadcaster_service import BroadcasterService
 from ..errors import BroadcasterNotFound
 from ..storage.model.twitch_broadcaster import TwitchBroadcaster
 from ..storage.unit_of_work import UnitOfWork
@@ -10,28 +10,23 @@ from twitchio import User
 
 
 class TwitchUserIdResolver(ErrorArgResolver[str, str]):
-    def __init__(self, uow: UnitOfWork = None, broadcaster_service: "BroadcasterService" = None):
+    def __init__(self, uow: UnitOfWork, broadcaster_service: BroadcasterService):
         self.uow = uow
         self.broadcaster_service = broadcaster_service
 
     async def resolve_detailed(self, argument: int, detailed: bool = True) -> str:
-        if self.uow is not None:
-            user: Optional[TwitchBroadcaster] = await self.uow.twitch_broadcasters.get_by_id(argument)
-            if user is not None:
-                return f"{user}"
+        user: Optional[TwitchBroadcaster] = await self.uow.twitch_broadcasters.get_by_id(argument)
+        if user is not None:
+            return f"{user}"
 
-        if self.broadcaster_service is not None:
-            try:
-                user: User = await self.broadcaster_service.fetch_user(user_id=argument)
-            except BroadcasterNotFound:
-                user = None
-            if user is not None:
-                return f"{user}" + \
-                       " (Not in DB)" * detailed
-
-        return "Twitch user with ID " * detailed + \
-               f"{argument}" + \
-               " (Not found in DB nor on Twitch)" * detailed
+        try:
+            user: User = await self.broadcaster_service.fetch_user(user_id=argument)
+            return f"{user}" + \
+                   " (Not in DB)" * detailed
+        except BroadcasterNotFound:
+            return "Twitch user with ID " * detailed + \
+                   f"{argument}" + \
+                   " (Not found in DB nor on Twitch)" * detailed
 
     async def resolve(self, argument: int) -> str:
         return await self.resolve_detailed(argument, False)
