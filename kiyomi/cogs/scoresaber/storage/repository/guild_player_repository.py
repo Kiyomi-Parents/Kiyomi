a3019d1d0a1ca7b6e28708f06ec6a3596806a09a
LuCkEr <lucker@lucker.xyz>
2022-07-18 06:06:27 +0300

    Removed extra eager loading

--- kiyomi/cogs/scoresaber/storage/repository/guild_player_repository.py
+++ kiyomi/cogs/scoresaber/storage/repository/guild_player_repository.py
@@ -2,8 +2,6 @@ import logging
 from typing import Optional, List, Type
 
 from sqlalchemy import select, delete
-from sqlalchemy.orm import joinedload
-from sqlalchemy.sql import Executable
 
 from kiyomi.database import BaseStorageRepository
 from ..model.guild_player import GuildPlayer
@@ -16,32 +14,20 @@ class GuildPlayerRepository(BaseStorageRepository[GuildPlayer]):
     def _table(self) -> Type[GuildPlayer]:
         return GuildPlayer
 
-    @staticmethod
-    def _eager_load_all(stmt: Executable):
-        stmt.options(
-            joinedload(GuildPlayer.guild),
-            joinedload(GuildPlayer.member),
-            joinedload(GuildPlayer.player),
-        )
-
     async def get_all_by_guild_id(self, guild_id: int) -> List[GuildPlayer]:
         stmt = select(self._table).where(self._table.guild_id == guild_id)
-        self._eager_load_all(stmt)
         return await self._all(stmt)
 
     async def get_all_by_member_id(self, member_id: int) -> List[GuildPlayer]:
         stmt = select(self._table).where(self._table.member_id == member_id)
-        self._eager_load_all(stmt)
         return await self._all(stmt)
 
     async def get_by_guild_id_and_player_id(self, guild_id: int, player_id: str) -> Optional[GuildPlayer]:
         stmt = select(self._table).where(self._table.guild_id == guild_id).where(self._table.player_id == player_id)
-        self._eager_load_all(stmt)
         return await self._first(stmt)
 
     async def get_by_guild_id_and_member_id(self, guild_id: int, member_id: int) -> Optional[GuildPlayer]:
         stmt = select(self._table).where(self._table.guild_id == guild_id).where(self._table.member_id == member_id)
-        self._eager_load_all(stmt)
         return await self._first(stmt)
 
     async def get_by_guild_id_and_member_id_and_player_id(
@@ -53,7 +39,6 @@ class GuildPlayerRepository(BaseStorageRepository[GuildPlayer]):
             .where(self._table.member_id == member_id)
             .where(self._table.player_id == player_id)
         )
-        self._eager_load_all(stmt)
         return await self._first(stmt)
 
     async def remove_by_guild_id_and_member_id_and_player_id(
--- kiyomi/cogs/twitch/storage/repository/guild_twitch_broadcaster_repository.py
+++ kiyomi/cogs/twitch/storage/repository/guild_twitch_broadcaster_repository.py
@@ -2,8 +2,6 @@ import logging
 from typing import Type, List, Optional
 
 from sqlalchemy import select, delete
-from sqlalchemy.orm import selectinload
-from sqlalchemy.sql import Executable
 
 from kiyomi.database import BaseStorageRepository
 from ..model.guild_twitch_broadcaster import GuildTwitchBroadcaster
@@ -12,28 +10,20 @@ _logger = logging.getLogger(__name__)
 
 
 class GuildTwitchBroadcasterRepository(BaseStorageRepository[GuildTwitchBroadcaster]):
-    def _eager_load_all(self, stmt: Executable):
-        return stmt.options(
-            selectinload(self._table.guild), selectinload(self._table.member), selectinload(self._table.twitch_broadcaster)
-        )
-
     @property
     def _table(self) -> Type[GuildTwitchBroadcaster]:
         return GuildTwitchBroadcaster
 
     async def get_by_guild_id_and_member_id(self, guild_id: int, member_id: int) -> Optional[GuildTwitchBroadcaster]:
         stmt = select(self._table).where(self._table.guild_id == guild_id).where(self._table.member_id == member_id)
-        stmt = self._eager_load_all(stmt)
         return await self._first(stmt)
 
     async def get_all_by_member_id(self, member_id: int) -> List[GuildTwitchBroadcaster]:
         stmt = select(self._table).where(self._table.member_id == member_id)
-        stmt = self._eager_load_all(stmt)
         return await self._all(stmt)
 
     async def get_all_by_broadcaster_id(self, broadcaster_id: str) -> List[GuildTwitchBroadcaster]:
         stmt = select(self._table).where(self._table.twitch_broadcaster_id == broadcaster_id)
-        stmt = self._eager_load_all(stmt)
         return await self._all(stmt)
 
     async def delete_by_guild_id_and_member_id(self, guild_id: int, member_id: int) -> Optional[GuildTwitchBroadcaster]:
